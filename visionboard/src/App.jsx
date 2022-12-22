354a135866d67730ec7415a8aae7e2e2d07fef20
ashleyCaples <108759933+ashleyCaples@users.noreply.github.com>
2022-12-21 20:24:18 -0500

    adding goals and pictures

--- visionboard/src/App.jsx
+++ visionboard/src/App.jsx
@@ -1,34 +1,236 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <head>
+        <title>the vision</title>
+      </head>
+      <div className="navbar">
+        <ul>
+          <li>
+            <a className="active" href="#home">
+              home
+            </a>
+          </li>
+          <li>
+            <a href="#locs">locs</a>
+          </li>
+          <li>
+            <a href="#career">career</a>
+          </li>
+          <li>
+            <a href="#skillhabit">skills & habits</a>
+          </li>
+          <li>
+            <a href="#travel">travel</a>
+          </li>
+          <li>
+            <a href="#aesthetics">aesthetics</a>
+          </li>
+          <li>
+            <a href="#fitnesshealth">fitness & health</a>
+          </li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
+      <header>
+        <h1>visionboard 2023</h1>
+      </header>
+      <main>
+        <div id="locs">
+          <article>
+            <h2>locs</h2>
+            <p>i want happy healthy locs</p>
+          </article>
+        </div>
+        <figure>
+          <img src="https://i.pinimg.com/564x/90/0d/8a/900d8a29b2ab0606a8760ba8d959a0fc.jpg" />
+          <img src="https://i.pinimg.com/564x/09/13/65/09136553287cb50d36eba6a23625fe4b.jpg" />
+          <img src="https://i.pinimg.com/564x/a2/f2/2e/a2f22ef697d714214c87d29a6063153a.jpg" />
+          <img src="https://i.pinimg.com/564x/85/d8/82/85d882c5a2bd635cb3bbc2624f4ea77f.jpg" />
+          <img src="https://i.pinimg.com/564x/3d/db/da/3ddbda1795aa4f22fc947b3bb45e306b.jpg" />
+          <img src="https://i.pinimg.com/564x/4a/c3/31/4ac33158fa1f9d52246d5b6b0668ac17.jpg" />
+          <img src="https://i.pinimg.com/564x/74/3d/4f/743d4f06c1375f5424d4a63acb3cccd6.jpg" />
+          <img src="https://i.pinimg.com/564x/ee/58/01/ee580183d0a1f6086943b574e6e58f8e.jpg" />
+          <img src="https://i.pinimg.com/564x/d8/72/7d/d8727df60cad628847807f91cc3921a7.jpg" />
+        </figure>
+        <div id="career">
+          <article>
+            <h2>career</h2>
+            <h3>growth & learning</h3>
+            <p>
+              <ul>
+                <li>finish a full course for web development</li>
+                <li>get a new job</li>
+                <li>develop a new skill every quarter (every 3 months)</li>
+              </ul>
+            </p>
+            <h3>networking</h3>
+            <p>
+              <ul>
+                <li>i will improve my networking skills</li>
+                <ol>
+                  <li>keep in touch with old peers</li>
+                  <li>become more active on linkedin</li>
+                </ol>
+                <li>i want to go to a conference this year</li>
+              </ul>
+            </p>
+          </article>
+        </div>
+        <figure>
+          <img src="https://i.pinimg.com/564x/e2/3e/e4/e23ee4b2012484c613c87f674a11353b.jpg" />
+          <img src="https://i.pinimg.com/564x/d8/c0/84/d8c084d082f3609035268af00ebbb3aa.jpg" />
+          <img src="https://i.pinimg.com/564x/08/70/c0/0870c06d6a0b9fe7eff0894ae02d9f46.jpg" />
+          <img src="https://i.pinimg.com/564x/23/fe/ee/23feeeed9086fd384a8f48bc84f7a526.jpg" />
+          <img src="https://i.pinimg.com/564x/83/bc/fd/83bcfdc0dacd9adc9d42028806231cee.jpg" />
+          <img src="https://i.pinimg.com/564x/ff/5f/d1/ff5fd1f527133567ea4da08d2b8f9fc4.jpg" />
+          <img src="https://media.istockphoto.com/id/1286022854/photo/african-american-woman-writing-computer-code.jpg?s=612x612&w=0&k=20&c=DUGZgnFzzymiccxKKoeFDFO-nDtNUL5rVxpbt2IYk4g=" />
+          <img src="" />
+          <img src="" />
+          <img src="" />
+          <img src="" />
+          <img src="" />
+          <img src="" />
+          <img src="" />
+        </figure>
+        <div id="skillhabit">
+          <article>
+            <h2>skills & habits</h2>
+            <h3>cooking</h3>
+            <p>
+              <ul>
+                <li>i will cook at least 5 times a week </li>
+                <li>
+                  i will try at least one new recipe every other week (at least
+                  26 new recipes put into my recipe book)
+                </li>
+                <li>
+                  i will create a recipe book (online or hardcopy) reading
+                </li>
+              </ul>
+            </p>
+            <h3>reading</h3>
+            <p>
+              i will read ONE book each month (a total of 12 books in the year)
+            </p>
+            <h3>habits</h3>
+            <p>
+              <ul>
+                <li>
+                  i will seize the most out of the day, i will wake up at 7:30 -
+                  8:00 am
+                </li>
+                <li>i will drink at least 60 oz of water </li>
+              </ul>
+            </p>
+          </article>
+        </div>
+        <figure>
+          <img src="https://i.pinimg.com/564x/5c/3f/11/5c3f11e79b5e0818726ada3802d7d77d.jpg" />
+          <img src="https://i.pinimg.com/564x/4e/10/6c/4e106c221030c9219bef559f8917b3c5.jpg" />
+          <img src="https://i.pinimg.com/564x/bf/f7/c9/bff7c978be7c05f99e21a36b87b63774.jpg" />
+          <img src="https://i.pinimg.com/564x/7d/f6/ec/7df6eceb7762a2483ab74fd6adacd746.jpg" />
+          <img src="https://i.pinimg.com/564x/be/80/66/be8066928b3eeb36fe6db935da21618c.jpg" />
+          <img src="https://i.pinimg.com/564x/e7/fc/00/e7fc00c30f8a7f983d12023634ac86cc.jpg" />
+          <img src="https://i.pinimg.com/564x/4b/61/2f/4b612f567c71882fd5e5b9d97a2d69e2.jpg" />
+          <img src="https://i.pinimg.com/564x/b6/54/c8/b654c8cd7264702fc5a6d50f11c46589.jpg" />
+          <img src="https://i.pinimg.com/564x/cc/ef/83/ccef83bf471e8d7feba4fd979d846f36.jpg" />
+          <img src="https://i.pinimg.com/564x/28/99/f8/2899f865e2caf2f9d93bec4bb635adf9.jpg" />
+          <img src="https://i.pinimg.com/564x/16/9f/1e/169f1e0fbfe602efe3ccc04c32b370cd.jpg" />
+          <img src="https://i.pinimg.com/564x/2d/8d/39/2d8d39de909b31298ca3dfe3ecdf312f.jpg" />
+          <img src="https://i.pinimg.com/564x/6d/d3/a5/6dd3a57963277c2b0f6634b445fdc467.jpg" />
+        </figure>
+        <div id="travel">
+          <article>
+            <h2>travel</h2>
+            <p>
+              <ul>
+                <li>i will go to New York for the first time</li>
+                <li>i will go to SOS SZA concert</li>
+                <li>i will go to a music festival</li>
+                <li>i will have a bomb ass birthday trip</li>
+              </ul>
+            </p>
+          </article>
+          <figure>
+            <img src="https://i.pinimg.com/564x/2b/e7/7d/2be77ddc210def9ed9440399102068af.jpg" />
+            <img src="https://i.pinimg.com/564x/92/d5/c8/92d5c84077a0da618d4c8a0515b123ed.jpg" />
+            <img src="https://i.pinimg.com/564x/6d/03/f8/6d03f86cf6d35c1ff235b809bd695e84.jpg" />
+            <img src="https://i.pinimg.com/564x/82/9c/78/829c789b08f47740cc928f23eca6fa76.jpg" />
+            <img src="https://i.pinimg.com/564x/d2/5f/04/d25f0473c71431f0332fd628080208bf.jpg" />
+            <img src="https://i.pinimg.com/564x/04/34/e8/0434e815a57a4894fd44da2e79a6bd4f.jpg" />
+            <img src="https://i.pinimg.com/236x/5a/e1/fd/5ae1fdb702715de06eb7f9b2e375e9dd.jpg" />
+          </figure>
+        </div>
+        <div id="aesthetics">
+          <article>
+            <h2>aesthetics</h2>
+            <h3>tattoos & piercing</h3>
+            <p>
+              <ul>
+                <li>i will start my sleeve of tattoos on my right arm</li>
+                <li>i will get a tattoo dedicated to Omari</li>
+                <li>i will re-pierce my ears and get new jewelry</li>
+              </ul>
+            </p>
+            <h3>the closet</h3>
+            <p>
+              <ul>
+                <li>i will buy myself a NICE chain</li>
+                <li>i will buy myself a NICE anklet </li>
+              </ul>
+            </p>
+            <h3>hobbies</h3>
+            <p>
+              <ul>
+                <li>i will try wheel throwing</li>
+                <li>other things to try: pottery, animation, 3D printing</li>
+              </ul>
+            </p>
+          </article>
+        </div>
+        <div id="finances">
+          <article>
+            <h2>finances</h2>
+            <p>
+              <ul>
+                <li>i will open a high yield savings account</li>
+                <li>i will begin investing in my roth ira </li>
+                <li>invest in S&P 500</li>
+                <li>i will save $5000 dollars </li>
+                <li>i will pay off my discover student loan </li>
+                <li>i will start planning or saving for a trip to take my parents on </li>
+              </ul>
+            </p>
+          </article>
+        </div>
+        <div id="fitnesshealth">
+          <article>
+            <h2>fitness & health</h2>
+            <p>
+              <ul>
+                <li>i will workout at least 4 to 5 days</li>
+                <li>i will build back muscles</li>
+                <li>i will tone my body</li>
+                <li>i will stay trying new and fun active things</li>
+                <ol>
+                  <li>january to april - stay consistent with 9round</li>
+                  <li>may to august - pilates</li>
+                  <li>other things to try: rock climbing, calisthenics, yoga, orange theory, train for a marathon</li>
+                </ol>
+              </ul>
+            </p>
+          </article>
+        </div>
+      </main>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p>this will be the year of memories and growth</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
