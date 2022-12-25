fdaa4f438d5928f91311178496b5c010ea33e0de
ashleyCaples <108759933+ashleyCaples@users.noreply.github.com>
2022-12-25 12:20:01 -0500

    adding picture

--- visionboard/src/App.jsx
+++ visionboard/src/App.jsx
@@ -30,6 +30,9 @@ function App() {
             <a href="#aesthetics">aesthetics</a>
           </li>
           <li>
+            <a href="#finances">finances</a>
+          </li>
+          <li>
             <a href="#fitnesshealth">fitness & health</a>
           </li>
         </ul>
@@ -42,18 +45,22 @@ function App() {
           <article>
             <h2>locs</h2>
             <p>i want happy healthy locs</p>
+            <p>first year loc'd</p>
           </article>
         </div>
         <figure>
-          <img src="https://i.pinimg.com/564x/90/0d/8a/900d8a29b2ab0606a8760ba8d959a0fc.jpg" />
-          <img src="https://i.pinimg.com/564x/09/13/65/09136553287cb50d36eba6a23625fe4b.jpg" />
-          <img src="https://i.pinimg.com/564x/a2/f2/2e/a2f22ef697d714214c87d29a6063153a.jpg" />
-          <img src="https://i.pinimg.com/564x/85/d8/82/85d882c5a2bd635cb3bbc2624f4ea77f.jpg" />
-          <img src="https://i.pinimg.com/564x/3d/db/da/3ddbda1795aa4f22fc947b3bb45e306b.jpg" />
-          <img src="https://i.pinimg.com/564x/4a/c3/31/4ac33158fa1f9d52246d5b6b0668ac17.jpg" />
-          <img src="https://i.pinimg.com/564x/74/3d/4f/743d4f06c1375f5424d4a63acb3cccd6.jpg" />
-          <img src="https://i.pinimg.com/564x/ee/58/01/ee580183d0a1f6086943b574e6e58f8e.jpg" />
-          <img src="https://i.pinimg.com/564x/d8/72/7d/d8727df60cad628847807f91cc3921a7.jpg" />
+          <div className="pics">
+            <img src="https://i.pinimg.com/564x/90/0d/8a/900d8a29b2ab0606a8760ba8d959a0fc.jpg" />
+            <img src="https://i.pinimg.com/564x/09/13/65/09136553287cb50d36eba6a23625fe4b.jpg" />
+            <img src="https://i.pinimg.com/564x/a2/f2/2e/a2f22ef697d714214c87d29a6063153a.jpg" />
+            <img src="https://i.pinimg.com/564x/85/d8/82/85d882c5a2bd635cb3bbc2624f4ea77f.jpg" />
+            <img src="https://i.pinimg.com/564x/3d/db/da/3ddbda1795aa4f22fc947b3bb45e306b.jpg" />
+            <img src="https://i.pinimg.com/564x/4a/c3/31/4ac33158fa1f9d52246d5b6b0668ac17.jpg" />
+            <img src="https://i.pinimg.com/564x/74/3d/4f/743d4f06c1375f5424d4a63acb3cccd6.jpg" />
+            <img src="https://i.pinimg.com/564x/ee/58/01/ee580183d0a1f6086943b574e6e58f8e.jpg" />
+            <img src="https://i.pinimg.com/564x/d8/72/7d/d8727df60cad628847807f91cc3921a7.jpg" />
+            <img src="https://i.pinimg.com/736x/36/7f/cf/367fcfee2134a2e73a31dd22f9c1580a.jpg" />
+          </div>
         </figure>
         <div id="career">
           <article>
@@ -80,13 +87,21 @@ function App() {
           </article>
         </div>
         <figure>
-          <img src="https://i.pinimg.com/564x/e2/3e/e4/e23ee4b2012484c613c87f674a11353b.jpg" />
-          <img src="https://i.pinimg.com/564x/d8/c0/84/d8c084d082f3609035268af00ebbb3aa.jpg" />
-          <img src="https://i.pinimg.com/564x/08/70/c0/0870c06d6a0b9fe7eff0894ae02d9f46.jpg" />
-          <img src="https://i.pinimg.com/564x/23/fe/ee/23feeeed9086fd384a8f48bc84f7a526.jpg" />
-          <img src="https://i.pinimg.com/564x/83/bc/fd/83bcfdc0dacd9adc9d42028806231cee.jpg" />
-          <img src="https://i.pinimg.com/564x/ff/5f/d1/ff5fd1f527133567ea4da08d2b8f9fc4.jpg" />
-          <img src="https://media.istockphoto.com/id/1286022854/photo/african-american-woman-writing-computer-code.jpg?s=612x612&w=0&k=20&c=DUGZgnFzzymiccxKKoeFDFO-nDtNUL5rVxpbt2IYk4g=" />
+          <div className="pics">
+            <img src="https://i.pinimg.com/564x/e2/3e/e4/e23ee4b2012484c613c87f674a11353b.jpg" />
+            <img src="https://i.pinimg.com/564x/d8/c0/84/d8c084d082f3609035268af00ebbb3aa.jpg" />
+            <img src="https://i.pinimg.com/564x/08/70/c0/0870c06d6a0b9fe7eff0894ae02d9f46.jpg" />
+            <img src="https://i.pinimg.com/564x/23/fe/ee/23feeeed9086fd384a8f48bc84f7a526.jpg" />
+            <img src="https://i.pinimg.com/564x/83/bc/fd/83bcfdc0dacd9adc9d42028806231cee.jpg" />
+            <img src="https://i.pinimg.com/564x/ff/5f/d1/ff5fd1f527133567ea4da08d2b8f9fc4.jpg" />
+            <img src="https://media.istockphoto.com/id/1286022854/photo/african-american-woman-writing-computer-code.jpg?s=612x612&w=0&k=20&c=DUGZgnFzzymiccxKKoeFDFO-nDtNUL5rVxpbt2IYk4g=" />
+            <img src="https://theartcareerproject.com/wp-content/uploads/2021/06/Web-Developer-Image.png" />
+            <img src="https://i.pinimg.com/564x/a0/f0/e2/a0f0e20fbef61da1145fbef70f072d21.jpg" />
+            <img src="https://i.pinimg.com/564x/b4/33/4a/b4334a4b0cb5a0364ff03372ec517c0c.jpg" />
+            <img src="https://i.pinimg.com/564x/a3/49/31/a34931decec5d46faa5fe8b4f79dfa77.jpg" />
+            <img src="https://imdiversity.com/wp-content/uploads/signing-a-contract-Article-201906071638-2.jpg" />
+            <img src="https://tpinsights.com/wp-content/uploads/2022/05/6118332c92ac1c1e139b7a55_black-tech-conferences-e1562423821812.jpeg" />
+          </div>
         </figure>
         <div id="skillhabit">
           <article>
@@ -121,20 +136,25 @@ function App() {
           </article>
         </div>
         <figure>
-          <img src="https://i.pinimg.com/564x/5c/3f/11/5c3f11e79b5e0818726ada3802d7d77d.jpg" />
-          <img src="https://i.pinimg.com/564x/4e/10/6c/4e106c221030c9219bef559f8917b3c5.jpg" />
-          <img src="https://i.pinimg.com/564x/bf/f7/c9/bff7c978be7c05f99e21a36b87b63774.jpg" />
-          <img src="https://i.pinimg.com/564x/7d/f6/ec/7df6eceb7762a2483ab74fd6adacd746.jpg" />
-          <img src="https://i.pinimg.com/564x/be/80/66/be8066928b3eeb36fe6db935da21618c.jpg" />
-          <img src="https://i.pinimg.com/564x/e7/fc/00/e7fc00c30f8a7f983d12023634ac86cc.jpg" />
-          <img src="https://i.pinimg.com/564x/4b/61/2f/4b612f567c71882fd5e5b9d97a2d69e2.jpg" />
-          <img src="https://i.pinimg.com/564x/b6/54/c8/b654c8cd7264702fc5a6d50f11c46589.jpg" />
-          <img src="https://i.pinimg.com/564x/cc/ef/83/ccef83bf471e8d7feba4fd979d846f36.jpg" />
-          <img src="https://i.pinimg.com/564x/28/99/f8/2899f865e2caf2f9d93bec4bb635adf9.jpg" />
-          <img src="https://i.pinimg.com/564x/16/9f/1e/169f1e0fbfe602efe3ccc04c32b370cd.jpg" />
-          <img src="https://i.pinimg.com/564x/2d/8d/39/2d8d39de909b31298ca3dfe3ecdf312f.jpg" />
-          <img src="https://i.pinimg.com/564x/6d/d3/a5/6dd3a57963277c2b0f6634b445fdc467.jpg" />
-          <img src="https://i.pinimg.com/564x/81/b0/b2/81b0b2fe042cbab276c088b8323de914.jpg" />
+          <div className="pics">
+            <img src="https://i.pinimg.com/564x/5c/3f/11/5c3f11e79b5e0818726ada3802d7d77d.jpg" />
+            <img src="https://i.pinimg.com/564x/4e/10/6c/4e106c221030c9219bef559f8917b3c5.jpg" />
+            <img src="https://i.pinimg.com/564x/bf/f7/c9/bff7c978be7c05f99e21a36b87b63774.jpg" />
+            <img src="https://i.pinimg.com/564x/8a/ed/99/8aed9944557bfc39f3871ba224c185f3.jpg"/>
+            <img src="https://i.pinimg.com/564x/7d/f6/ec/7df6eceb7762a2483ab74fd6adacd746.jpg" />
+            <img src="https://i.pinimg.com/564x/be/80/66/be8066928b3eeb36fe6db935da21618c.jpg" />
+            <img src="https://i.pinimg.com/564x/e7/fc/00/e7fc00c30f8a7f983d12023634ac86cc.jpg" />
+            <img src="https://i.pinimg.com/564x/4b/61/2f/4b612f567c71882fd5e5b9d97a2d69e2.jpg" />
+            <img src="https://i.pinimg.com/564x/b6/54/c8/b654c8cd7264702fc5a6d50f11c46589.jpg" />
+            <img src="https://i.pinimg.com/564x/cc/ef/83/ccef83bf471e8d7feba4fd979d846f36.jpg" />
+            <img src="https://i.pinimg.com/564x/28/99/f8/2899f865e2caf2f9d93bec4bb635adf9.jpg" />
+            <img src="https://i.pinimg.com/564x/16/9f/1e/169f1e0fbfe602efe3ccc04c32b370cd.jpg" />
+            <img src="https://i.pinimg.com/564x/2d/8d/39/2d8d39de909b31298ca3dfe3ecdf312f.jpg" />
+            <img src="https://i.pinimg.com/564x/6d/d3/a5/6dd3a57963277c2b0f6634b445fdc467.jpg" />
+            <img src="https://i.pinimg.com/564x/81/b0/b2/81b0b2fe042cbab276c088b8323de914.jpg" />
+            <img src="https://i.pinimg.com/564x/cc/36/a2/cc36a24389efe1f7bebfa8d955a3b59e.jpg" />
+            <img src="https://i.pinimg.com/564x/47/d9/c3/47d9c34fd5480ac7529240b5d7bbd2f2.jpg" />
+          </div>
         </figure>
         <div id="travel">
           <article>
@@ -149,13 +169,17 @@ function App() {
             </p>
           </article>
           <figure>
-            <img src="https://i.pinimg.com/564x/2b/e7/7d/2be77ddc210def9ed9440399102068af.jpg" />
-            <img src="https://i.pinimg.com/564x/92/d5/c8/92d5c84077a0da618d4c8a0515b123ed.jpg" />
-            <img src="https://i.pinimg.com/564x/6d/03/f8/6d03f86cf6d35c1ff235b809bd695e84.jpg" />
-            <img src="https://i.pinimg.com/564x/82/9c/78/829c789b08f47740cc928f23eca6fa76.jpg" />
-            <img src="https://i.pinimg.com/564x/d2/5f/04/d25f0473c71431f0332fd628080208bf.jpg" />
-            <img src="https://i.pinimg.com/564x/04/34/e8/0434e815a57a4894fd44da2e79a6bd4f.jpg" />
-            <img src="https://i.pinimg.com/236x/5a/e1/fd/5ae1fdb702715de06eb7f9b2e375e9dd.jpg" />
+            <div className="pics">
+              <img src="https://i.pinimg.com/564x/2b/e7/7d/2be77ddc210def9ed9440399102068af.jpg" />
+              <img src="https://i.pinimg.com/564x/92/d5/c8/92d5c84077a0da618d4c8a0515b123ed.jpg" />
+              <img src="https://i.pinimg.com/564x/6d/03/f8/6d03f86cf6d35c1ff235b809bd695e84.jpg" />
+              <img src="https://i.pinimg.com/564x/82/9c/78/829c789b08f47740cc928f23eca6fa76.jpg" />
+              <img src="https://i.pinimg.com/564x/d2/5f/04/d25f0473c71431f0332fd628080208bf.jpg" />
+              <img src="https://i.pinimg.com/564x/04/34/e8/0434e815a57a4894fd44da2e79a6bd4f.jpg" />
+              <img src="https://i.pinimg.com/236x/5a/e1/fd/5ae1fdb702715de06eb7f9b2e375e9dd.jpg" />
+              <img src="https://i.pinimg.com/564x/b2/bd/6e/b2bd6e112aed3e869480e7f39f1dbd39.jpg" />
+              <img src="https://i.pinimg.com/564x/9e/be/64/9ebe64e8abdcb21c14bab6a1fb43e6cb.jpg" />
+            </div>
           </figure>
         </div>
         <div id="aesthetics">
@@ -184,6 +208,45 @@ function App() {
               </ul>
             </p>
           </article>
+          <figure>
+            <div className="pics">
+              <img src="https://i.pinimg.com/564x/dd/28/4d/dd284db2a17fcf8a731eafe4ebb4c2dc.jpg" />
+              <img src="https://i.pinimg.com/564x/fa/a9/63/faa963b584c844b6ae3c10f8731c7534.jpg" />
+              <img src="https://i.pinimg.com/564x/db/0c/94/db0c94aadd22dad30ce2920dcad8220f.jpg" />
+              <img src="https://i.pinimg.com/564x/e3/1a/f5/e31af5615326514ef937aa435550cee2.jpg" />
+              <img src="https://i.pinimg.com/564x/b4/a5/b8/b4a5b869b568b91a17be81224bfbe0fb.jpg" />
+              <img src="https://i.pinimg.com/736x/c1/25/8c/c1258c2f8a0b3e3c05ffb1d04f529f41.jpg" />
+              <img src="https://i.pinimg.com/736x/07/97/4f/07974fc377f5c3ea0bec2ea08ae1b7f6.jpg" />
+              <img src="https://i.pinimg.com/564x/e6/34/c1/e634c17037c97b839f8348260232418f.jpg" />
+              <img src="https://i.pinimg.com/564x/2d/00/6f/2d006fbab5b96803600b4675e21391d8.jpg" />
+              <img src="https://i.pinimg.com/564x/99/90/10/999010e62c98e7632b3cbc5bd29a6161.jpg" />
+              <img src="https://i.pinimg.com/736x/01/1b/e8/011be8beea6007c162193b7c4bccf625.jpg" />
+              <img src="https://i.pinimg.com/564x/ad/75/2f/ad752f2f44887fe3a52e4db3e948fb76.jpg" />
+              <img src="https://i.pinimg.com/564x/ea/3d/fa/ea3dfab6807b91530596448b78cb2d75.jpg" />
+              <img src="https://i.pinimg.com/564x/79/30/31/7930318d421534ea92a3d965315a8e61.jpg" />
+              <img src="https://i.pinimg.com/564x/86/bc/18/86bc18d5edea01ff07f1246a181c4723.jpg" />
+              <img src="https://i.pinimg.com/564x/7c/94/dc/7c94dc1564425d70a405b8501532fc07.jpg" />
+              <img src="https://i.pinimg.com/564x/27/3e/d0/273ed0feb545e44f7e84855426500d58.jpg" />
+              <img src="https://i.pinimg.com/564x/2a/15/7f/2a157f331134b6066e1725ca2f2c8922.jpg" />
+              <img src="https://i.pinimg.com/564x/34/30/7d/34307df7bde0800dead91ce15d9f4e74.jpg" />
+              <img src="https://i.pinimg.com/564x/37/5a/3f/375a3f93ea70c079f8c8e4b287db8ce2.jpg" />
+              <img src="https://i.pinimg.com/564x/78/16/89/781689bbaae515ec9d389b9281563acc.jpg" />
+              <img src="https://i.pinimg.com/564x/38/be/00/38be008121ac73753ee827044246a8b2.jpg" />
+              <img src="https://i.pinimg.com/564x/d8/4a/89/d84a89cf7c9cbd9df5daee2eeb560c6a.jpg" />
+              <img src="https://i.pinimg.com/564x/2d/c2/11/2dc211b60a26864faca3ef50464c6df3.jpg" />
+              <img src="https://i.pinimg.com/564x/58/2d/68/582d688e35d39b495da4f87ae23b8fbe.jpg" />
+              <img src="https://i.pinimg.com/564x/a3/7d/4c/a37d4c1c06df8aa7452db7a97da89e23.jpg" />
+              <img src="https://i.pinimg.com/564x/bc/af/bb/bcafbbeac507cbb1edc72c7f7b30c20b.jpg" />
+              <img src="https://i.pinimg.com/564x/f6/90/b0/f690b0b85a587614ae5ba65bbefc15a3.jpg" />
+              <img src="https://i.pinimg.com/564x/56/91/48/5691489556e58c550b89a8f038f553b9.jpg" />
+              <img src="https://i.pinimg.com/564x/f0/a6/5e/f0a65e4192c0abeaa76c7763f89b3508.jpg" />
+              <img src="https://i.pinimg.com/564x/e8/f3/3c/e8f33c23771849a5d4acdea406a0df98.jpg" />
+              <img src="https://i.pinimg.com/564x/0d/c5/9f/0dc59f8c14a1b29c177cdf201f358f1e.jpg" />
+              <img src="https://i.pinimg.com/564x/91/36/9e/91369eb282f21886c7be688d60939643.jpg" />
+              <img src="https://i.pinimg.com/564x/a4/f7/5b/a4f75bf500b5f7525645ed01183f11b2.jpg" />
+              <img src="https://i.pinimg.com/750x/ac/a0/5e/aca05e706438dad740d324d8e026df01.jpg"/>
+            </div>
+          </figure>
         </div>
         <div id="finances">
           <article>
@@ -202,6 +265,20 @@ function App() {
               </ul>
             </p>
           </article>
+          <figure>
+            <div className="pics">
+              <img src="https://i.pinimg.com/736x/5f/e7/d7/5fe7d7593303dfb071b5bda7c58bff97.jpg" />
+              <img src="https://i.pinimg.com/564x/ef/fd/c5/effdc56836d925110c8a91f9f5440e4e.jpg" />
+              <img src="https://i.pinimg.com/736x/47/33/8e/47338ecdc9f1594bf7c64d0d3b441e1a.jpg" />
+              <img src="https://i.pinimg.com/564x/d1/76/8a/d1768a5ec24c51ddcaa08edf827acefb.jpg" />
+              <img src="https://i.pinimg.com/564x/d6/5d/78/d65d782bc6ce1f157c5f06204e7b1969.jpg" />
+              <img src="https://i.pinimg.com/564x/bf/19/53/bf19533413145f7918a55511da555d58.jpg" />
+              <img src="https://prodimage.images-bn.com/pimages/9780593232743_p6_v7_s600x595.jpg" />
+              <img src="https://i.pinimg.com/564x/52/28/df/5228df0eefdda91bf8619502258b0121.jpg" />
+              <img src="https://accidentallyretired.com/wp-content/uploads/2021/05/The-Little-Book-of-Common-Sense-Investing-by-John-C.-Bogle.jpg" />
+              <img src="https://m.foolcdn.com/media/dubs/original_images/SP_500_Index.jpg" />
+            </div>
+          </figure>
         </div>
         <div id="fitnesshealth">
           <article>
@@ -224,15 +301,19 @@ function App() {
             </p>
           </article>
           <figure>
-            <img src="https://i.pinimg.com/564x/cc/55/f3/cc55f303686b3acab488c3edc611034b.jpg"/>
-            <img src="https://i.pinimg.com/564x/2c/66/aa/2c66aa4f5c8bebcb1dcad9af14c98a1a.jpg"/>
-            <img src="https://i.pinimg.com/564x/ce/f3/4d/cef34dea5ea7c198d9457a21b5f364d5.jpg"/>
-            <img src="https://i.pinimg.com/564x/dd/1d/d0/dd1dd0e7626582927d9820903a55e4ae.jpg"/>
-            <img src="https://i.pinimg.com/564x/14/2b/94/142b9487830ee9c117fcedf07f443c4d.jpg"/>
-            <img src="https://i.pinimg.com/564x/1f/54/41/1f5441bfbc0479347b7036810854ad23.jpg"/>
-            <img src="https://i.pinimg.com/750x/77/7d/2b/777d2b0b91deb21e67463e084851a8fc.jpg"/>
-            <img src="https://i.pinimg.com/564x/92/6b/13/926b13a0be3674fb117305f60d174cec.jpg"/>
-            <img src="https://i.pinimg.com/564x/6a/d5/36/6ad5362a9970448e5be0c80fc58fe99f.jpg"/>
+            <div className="pics">
+              <img src="https://i.pinimg.com/564x/cc/55/f3/cc55f303686b3acab488c3edc611034b.jpg" />
+              <img src="https://i.pinimg.com/564x/2c/66/aa/2c66aa4f5c8bebcb1dcad9af14c98a1a.jpg" />
+              <img src="https://i.pinimg.com/564x/ce/f3/4d/cef34dea5ea7c198d9457a21b5f364d5.jpg" />
+              <img src="https://i.pinimg.com/564x/dd/1d/d0/dd1dd0e7626582927d9820903a55e4ae.jpg" />
+              <img src="https://i.pinimg.com/564x/14/2b/94/142b9487830ee9c117fcedf07f443c4d.jpg" />
+              <img src="https://i.pinimg.com/564x/1f/54/41/1f5441bfbc0479347b7036810854ad23.jpg" />
+              <img src="https://i.pinimg.com/750x/77/7d/2b/777d2b0b91deb21e67463e084851a8fc.jpg" />
+              <img src="https://i.pinimg.com/564x/92/6b/13/926b13a0be3674fb117305f60d174cec.jpg" />
+              <img src="https://i.pinimg.com/564x/6a/d5/36/6ad5362a9970448e5be0c80fc58fe99f.jpg" /> 
+              <img src="https://i.pinimg.com/564x/c6/64/27/c664273aecd3140e55ed81c7466c4281.jpg"/>
+              <img src="https://i.pinimg.com/564x/4a/5c/86/4a5c866f02e385722f12fedd37be986e.jpg"/>
+            </div>
           </figure>
         </div>
       </main>
