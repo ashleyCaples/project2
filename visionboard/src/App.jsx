1e38bb3095364ff9901839e80492a3e1f05fdd93
ashleyCaples <108759933+ashleyCaples@users.noreply.github.com>
2023-01-07 10:01:19 -0500

    css updates

--- visionboard/src/App.jsx
+++ visionboard/src/App.jsx
@@ -49,7 +49,7 @@ function App() {
           </article>
         </div>
         <figure>
-          <div className="pics">
+          <div>
             <img src="https://i.pinimg.com/564x/90/0d/8a/900d8a29b2ab0606a8760ba8d959a0fc.jpg" />
             <img src="https://i.pinimg.com/564x/09/13/65/09136553287cb50d36eba6a23625fe4b.jpg" />
             <img src="https://i.pinimg.com/564x/a2/f2/2e/a2f22ef697d714214c87d29a6063153a.jpg" />
@@ -67,7 +67,7 @@ function App() {
             <h2>career</h2>
             <h3>growth & learning</h3>
             <p>
-              <ul>
+              <ul className="goallist">
                 <li>finish a full course for web development</li>
                 <li>get a new job</li>
                 <li>develop a new skill every quarter (every 3 months)</li>
@@ -75,7 +75,7 @@ function App() {
             </p>
             <h3>networking</h3>
             <p>
-              <ul>
+              <ul className="goallist">
                 <li>i will improve my networking skills</li>
                 <ol>
                   <li>keep in touch with old peers</li>
@@ -87,7 +87,7 @@ function App() {
           </article>
         </div>
         <figure>
-          <div className="pics">
+          <div>
             <img src="https://i.pinimg.com/564x/e2/3e/e4/e23ee4b2012484c613c87f674a11353b.jpg" />
             <img src="https://i.pinimg.com/564x/d8/c0/84/d8c084d082f3609035268af00ebbb3aa.jpg" />
             <img src="https://i.pinimg.com/564x/08/70/c0/0870c06d6a0b9fe7eff0894ae02d9f46.jpg" />
@@ -101,6 +101,7 @@ function App() {
             <img src="https://i.pinimg.com/564x/a3/49/31/a34931decec5d46faa5fe8b4f79dfa77.jpg" />
             <img src="https://imdiversity.com/wp-content/uploads/signing-a-contract-Article-201906071638-2.jpg" />
             <img src="https://tpinsights.com/wp-content/uploads/2022/05/6118332c92ac1c1e139b7a55_black-tech-conferences-e1562423821812.jpeg" />
+            <img src="https://i0.wp.com/www.goingconcern.com/wp-content/uploads/2019/07/armanino.jpg?resize=885%2C500&ssl=1" />
           </div>
         </figure>
         <div id="skillhabit">
@@ -108,7 +109,7 @@ function App() {
             <h2>skills & habits</h2>
             <h3>cooking</h3>
             <p>
-              <ul>
+              <ul className="goallist">
                 <li>i will cook at least 5 times a week </li>
                 <li>
                   i will try at least one new recipe every other week (at least
@@ -125,7 +126,7 @@ function App() {
             </p>
             <h3>habits</h3>
             <p>
-              <ul>
+              <ul className="goallist">
                 <li>
                   i will seize the most out of the day, i will wake up at 7:30 -
                   8:00 am
@@ -136,11 +137,11 @@ function App() {
           </article>
         </div>
         <figure>
-          <div className="pics">
+          <div>
             <img src="https://i.pinimg.com/564x/5c/3f/11/5c3f11e79b5e0818726ada3802d7d77d.jpg" />
             <img src="https://i.pinimg.com/564x/4e/10/6c/4e106c221030c9219bef559f8917b3c5.jpg" />
             <img src="https://i.pinimg.com/564x/bf/f7/c9/bff7c978be7c05f99e21a36b87b63774.jpg" />
-            <img src="https://i.pinimg.com/564x/8a/ed/99/8aed9944557bfc39f3871ba224c185f3.jpg"/>
+            <img src="https://i.pinimg.com/564x/8a/ed/99/8aed9944557bfc39f3871ba224c185f3.jpg" />
             <img src="https://i.pinimg.com/564x/7d/f6/ec/7df6eceb7762a2483ab74fd6adacd746.jpg" />
             <img src="https://i.pinimg.com/564x/be/80/66/be8066928b3eeb36fe6db935da21618c.jpg" />
             <img src="https://i.pinimg.com/564x/e7/fc/00/e7fc00c30f8a7f983d12023634ac86cc.jpg" />
@@ -160,16 +161,16 @@ function App() {
           <article>
             <h2>travel</h2>
             <p>
-              <ul>
+              <ul className="goallist">
                 <li>i will go to New York for the first time</li>
-                <li>i will go to SOS SZA concert</li>
+                <li>i will go to the SOS SZA concert</li>
                 <li>i will go to a music festival</li>
                 <li>i will have a bomb ass birthday trip</li>
               </ul>
             </p>
           </article>
           <figure>
-            <div className="pics">
+            <div>
               <img src="https://i.pinimg.com/564x/2b/e7/7d/2be77ddc210def9ed9440399102068af.jpg" />
               <img src="https://i.pinimg.com/564x/92/d5/c8/92d5c84077a0da618d4c8a0515b123ed.jpg" />
               <img src="https://i.pinimg.com/564x/6d/03/f8/6d03f86cf6d35c1ff235b809bd695e84.jpg" />
@@ -187,7 +188,7 @@ function App() {
             <h2>aesthetics</h2>
             <h3>tattoos & piercing</h3>
             <p>
-              <ul>
+              <ul className="goallist">
                 <li>i will start my sleeve of tattoos on my right arm</li>
                 <li>i will get a tattoo dedicated to Omari</li>
                 <li>i will re-pierce my ears and get new jewelry</li>
@@ -195,21 +196,21 @@ function App() {
             </p>
             <h3>the closet</h3>
             <p>
-              <ul>
+              <ul className="goallist">
                 <li>i will buy myself a NICE chain</li>
                 <li>i will buy myself a NICE anklet </li>
               </ul>
             </p>
             <h3>hobbies</h3>
             <p>
-              <ul>
+              <ul className="goallist">
                 <li>i will try wheel throwing</li>
                 <li>other things to try: pottery, animation, 3D printing</li>
               </ul>
             </p>
           </article>
           <figure>
-            <div className="pics">
+            <div>
               <img src="https://i.pinimg.com/564x/dd/28/4d/dd284db2a17fcf8a731eafe4ebb4c2dc.jpg" />
               <img src="https://i.pinimg.com/564x/fa/a9/63/faa963b584c844b6ae3c10f8731c7534.jpg" />
               <img src="https://i.pinimg.com/564x/db/0c/94/db0c94aadd22dad30ce2920dcad8220f.jpg" />
@@ -244,7 +245,7 @@ function App() {
               <img src="https://i.pinimg.com/564x/0d/c5/9f/0dc59f8c14a1b29c177cdf201f358f1e.jpg" />
               <img src="https://i.pinimg.com/564x/91/36/9e/91369eb282f21886c7be688d60939643.jpg" />
               <img src="https://i.pinimg.com/564x/a4/f7/5b/a4f75bf500b5f7525645ed01183f11b2.jpg" />
-              <img src="https://i.pinimg.com/750x/ac/a0/5e/aca05e706438dad740d324d8e026df01.jpg"/>
+              <img src="https://i.pinimg.com/750x/ac/a0/5e/aca05e706438dad740d324d8e026df01.jpg" />
             </div>
           </figure>
         </div>
@@ -252,7 +253,7 @@ function App() {
           <article>
             <h2>finances</h2>
             <p>
-              <ul>
+              <ul className="goallist">
                 <li>i will open a high yield savings account</li>
                 <li>i will begin investing in my roth ira </li>
                 <li>invest in S&P 500</li>
@@ -260,13 +261,13 @@ function App() {
                 <li>i will pay off my discover student loan </li>
                 <li>
                   i will start planning or saving for a trip to take my parents
-                  on{" "}
+                  on
                 </li>
               </ul>
             </p>
           </article>
           <figure>
-            <div className="pics">
+            <div>
               <img src="https://i.pinimg.com/736x/5f/e7/d7/5fe7d7593303dfb071b5bda7c58bff97.jpg" />
               <img src="https://i.pinimg.com/564x/ef/fd/c5/effdc56836d925110c8a91f9f5440e4e.jpg" />
               <img src="https://i.pinimg.com/736x/47/33/8e/47338ecdc9f1594bf7c64d0d3b441e1a.jpg" />
@@ -277,15 +278,16 @@ function App() {
               <img src="https://i.pinimg.com/564x/52/28/df/5228df0eefdda91bf8619502258b0121.jpg" />
               <img src="https://accidentallyretired.com/wp-content/uploads/2021/05/The-Little-Book-of-Common-Sense-Investing-by-John-C.-Bogle.jpg" />
               <img src="https://m.foolcdn.com/media/dubs/original_images/SP_500_Index.jpg" />
-              <img src="https://thepointsguy.global.ssl.fastly.net/us/originals/2021/06/NYC-FiDi_Amex-Gold_1_WSmith.jpg"/>
+              <img src="https://thepointsguy.global.ssl.fastly.net/us/originals/2021/06/NYC-FiDi_Amex-Gold_1_WSmith.jpg" />
+              <img src="https://i.pinimg.com/564x/87/03/5a/87035a0bcc86b8f279cee9102d8e461d.jpg" />
             </div>
           </figure>
         </div>
-        <div id="fitnesshealth">
+        <div className="fitnesshealth">
           <article>
             <h2>fitness & health</h2>
             <p>
-              <ul>
+              <ul className="goallist">
                 <li>i will workout at least 4 to 5 days</li>
                 <li>i will build back muscles</li>
                 <li>i will tone my body</li>
@@ -302,7 +304,7 @@ function App() {
             </p>
           </article>
           <figure>
-            <div className="pics">
+            <div>
               <img src="https://i.pinimg.com/564x/cc/55/f3/cc55f303686b3acab488c3edc611034b.jpg" />
               <img src="https://i.pinimg.com/564x/2c/66/aa/2c66aa4f5c8bebcb1dcad9af14c98a1a.jpg" />
               <img src="https://i.pinimg.com/564x/ce/f3/4d/cef34dea5ea7c198d9457a21b5f364d5.jpg" />
@@ -311,11 +313,11 @@ function App() {
               <img src="https://i.pinimg.com/564x/1f/54/41/1f5441bfbc0479347b7036810854ad23.jpg" />
               <img src="https://i.pinimg.com/750x/77/7d/2b/777d2b0b91deb21e67463e084851a8fc.jpg" />
               <img src="https://i.pinimg.com/564x/92/6b/13/926b13a0be3674fb117305f60d174cec.jpg" />
-              <img src="https://i.pinimg.com/564x/6a/d5/36/6ad5362a9970448e5be0c80fc58fe99f.jpg" /> 
-              <img src="https://i.pinimg.com/564x/c6/64/27/c664273aecd3140e55ed81c7466c4281.jpg"/>
-              <img src="https://i.pinimg.com/564x/4a/5c/86/4a5c866f02e385722f12fedd37be986e.jpg"/>
-              <img src="https://iowacity.momcollective.com/wp-content/uploads/2022/03/IMG_0527.jpg"/>
-              <img src="https://i.pinimg.com/736x/2f/6b/67/2f6b678adc72d14c7846e12be1337268.jpg"/>
+              <img src="https://i.pinimg.com/564x/6a/d5/36/6ad5362a9970448e5be0c80fc58fe99f.jpg" />
+              <img src="https://i.pinimg.com/564x/c6/64/27/c664273aecd3140e55ed81c7466c4281.jpg" />
+              <img src="https://i.pinimg.com/564x/4a/5c/86/4a5c866f02e385722f12fedd37be986e.jpg" />
+              <img src="https://iowacity.momcollective.com/wp-content/uploads/2022/03/IMG_0527.jpg" />
+              <img src="https://i.pinimg.com/736x/2f/6b/67/2f6b678adc72d14c7846e12be1337268.jpg" />
             </div>
           </figure>
         </div>
